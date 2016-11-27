caac467e1b4bad4b1ac289f181d8e02ffd949f28
Spencer Dixon <spencercdixon@gmail.com>
2016-11-27 12:09:08 -0500

    Adds setting page to analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,6 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('UA-51806130-1');
 
 exports.onRouteUpdate = (state, page, pages) => {
+  ReactGA.set({page: state.pathname});
   ReactGA.pageview(state.pathname);
 };
