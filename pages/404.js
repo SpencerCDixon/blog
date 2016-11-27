22fab0ea6dcc39e64a9c1e06ec1d844cd548f96b
Spencer Dixon <spencercdixon@gmail.com>
2016-11-27 12:00:47 -0500

    Fixes 404 error page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+
+function FourOhFour({  }) {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>404 NOT FOUND</h1>
+      <h2>Whoops this is awkward.</h2>
+    </div>
+  );
+}
+
+FourOhFour.propTypes = {
+};
+export default FourOhFour;
